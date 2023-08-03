405ea0c647f05baac0b8dc4a19a136c52253dc6f
Öner Taylan Öztürk <onertaylanozturk@gmail.com>
2023-08-03 14:25:27 +0300

    react query

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://osbmfjwljmsobritppin.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zYm1mandsam1zb2JyaXRwcGluIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA5MTc1MDAsImV4cCI6MjAwNjQ5MzUwMH0.kXGJfD0s5hvVEaA7WTOW2N_-r6RYlkhWUDzX1Wbmm5k";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
