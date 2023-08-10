2db3bff5d16886194afaec56fd897ea757ebb655
Öner Taylan Öztürk <onertaylanozturk@gmail.com>
2023-08-10 22:59:14 +0300

    booking table

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -1,10 +1,13 @@
 import BookingRow from "./BookingRow";
+import { useBookings } from "./useBookings";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import Spinner from "../../ui/Spinner";
 
 function BookingTable() {
-  const bookings = [];
+  const { bookings, isLoading } = useBookings();
 
+  if (isLoading) return <Spinner />;
   return (
     <Menus>
       <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,12 +1,20 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import BookingTable from "../features/bookings/BookingTable";
+import Empty from "../ui/Empty";
 
 function Bookings() {
+  const bookings = [];
+
+  if (!bookings) return <Empty resourceName="bookings"></Empty>;
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All bookings</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All bookings</Heading>
+      </Row>
+
+      <BookingTable />
+    </>
   );
 }
 
